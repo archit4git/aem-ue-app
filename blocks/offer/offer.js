bbc2ef8a7a10ca9e8ba299d663527dce3761d215
archit4git <archit.garg@gmail.com>
2025-08-15 11:49:39 -0400

    Added offer block

---
+++ blocks/offer/offer.js
@@ -0,0 +1,40 @@
+export default async function decorate(block) {
+  const aempublishurl = 'https://publish-p14733-e1160558.adobeaemcloud.com';
+  const aemauthorurl = 'https://author-p14733-e1160558.adobeaemcloud.com';
+  const persistedquery = '/graphql/execute.json/ag-eds-site-4/OfferByPath';
+  const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
+  let variationname = 'main';
+  const variationElem = block.querySelector(':scope div:nth-child(2) > div > p');
+  if (variationElem && variationElem.innerHTML) {
+    variationname = variationElem.innerHTML.trim();
+  }
+
+  const url = window.location && window.location.origin && window.location.origin.includes('author')
+    ? `${aemauthorurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`
+    : `${aempublishurl}${persistedquery};path=${offerpath};variation=${variationname};ts=${Math.random() * 1000}`;
+  const options = { credentials: 'include' };
+
+  const cfReq = await fetch(url, options)
+    .then((response) => response.json())
+    .then((contentfragment) => {
+      let offer = '';
+      if (contentfragment.data) {
+        offer = contentfragment.data.offerByPath.item;
+      }
+      return offer;
+    });
+
+  const itemId = `urn:aemconnection:${offerpath}/jcr:content/data/master`;
+
+  block.innerHTML = `
+  <div class='offer-content' data-aue-resource=${itemId} data-aue-label="offer content fragment" data-aue-type="reference" data-aue-filter="cf">
+      <div class='offer-left'>
+          <h4 data-aue-prop="title" data-aue-label="title" data-aue-type="text" class='title'>${cfReq.title}</h4>
+          <p data-aue-prop="content" data-aue-label="content" data-aue-type="text" class='content'>${cfReq.content.plaintext}</p>
+      </div>
+      <div class='offer-right'>
+         <a href="${cfReq.ctaUrl}" data-aue-prop="callToAction" data-aue-label="Call to Action" data-aue-type="text" class='button secondary'>${cfReq.callToAction}</a>
+      </div>
+  </div>
+`;
+}
